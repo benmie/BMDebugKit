2fd11de91393eb740041a48b2433bc532bfab0cf
Benjamin Mies <mib@ti8m.ch>
2018-12-05 17:36:00 +0100

    Add Xcode project

---
+++ BMDebugKit/BMDebugKit/BMDebugKit.h
@@ -0,0 +1,19 @@
+//
+//  BMDebugKit.h
+//  BMDebugKit
+//
+//  Created by Benjamin Mies on 05.12.18.
+//  Copyright © 2018 Benjamin Mies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMDebugKit.
+FOUNDATION_EXPORT double BMDebugKitVersionNumber;
+
+//! Project version string for BMDebugKit.
+FOUNDATION_EXPORT const unsigned char BMDebugKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMDebugKit/PublicHeader.h>
+
+
